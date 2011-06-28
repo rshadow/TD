53ed97eb84c7ff4d094bda4e113772dfca655e8e
rshadow <rshadow@rambler.ru>
2011-06-28 17:24:52 +0400

    new tower object

---
+++ lib/Game/TD/Model/Tower.pm
@@ -0,0 +1,54 @@
+use strict;
+use warnings;
+use utf8;
+
+package Game::TD::Model::Tower;
+#use base qw(Exporter);
+#our @EXPORT = qw();
+
+use Carp;
+use Game::TD::Config;
+
+=head1 Game::TD::Model::Tower
+
+Описание_модуля
+
+=cut
+
+=head1 Функции
+
+=cut
+
+=head2 new HASH
+
+Конструктор
+
+=head3 Входные параметры
+
+=head4 параметр
+
+=cut
+
+sub new
+{
+    my ($class, %opts) = @_;
+
+    croak 'Missing required param "type"'   unless defined $opts{type};
+
+    my $self = bless \%opts, $class;
+
+    # Get from config
+    my %tower = %{ config->param('tower'=>'towers'=>$self->type) || {} };
+    %tower = %{ config->param('tower'=>'unknown') } unless %tower;
+    # Concat
+    $self->{$_} = $tower{$_} for keys %tower;
+
+    return $self;
+}
+
+sub damage { return shift()->{damage} }
+sub speed  { return shift()->{speed}  }
+sub type   { return shift()->{type}   }
+sub item   { return shift()->{item}   }
+
+1;
